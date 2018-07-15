c82b5f052714946d74eab091b0e37b693c94e3e3
Zenn <app.zenn@gmail.com>
2018-07-16 02:11:08 +0900

    [routerLink] の使いどころ

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  id = '100';
 
   constructor() { }
 
