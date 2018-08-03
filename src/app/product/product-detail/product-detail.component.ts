87ffea092b227c0e73efa2d834f4d4527421ea63
Zenn <app.zenn@gmail.com>
2018-08-04 00:47:40 +0900

    詳細画面、編集画面で、URLのパラメータからどの商品かを判断するようにする

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { ProductService } from '../../shared/services/product.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,13 +12,15 @@ export class ProductDetailComponent implements OnInit {
   product: Product;
 
   constructor(
+    private route: ActivatedRoute,
     private productService: ProductService,
   ) {}
 
   ngOnInit() {
-    this.productService.get(2).subscribe((product: Product) => {
-      this.product = product;
+    this.route.params.subscribe((params: Params) => {
+      this.productService.get(params['id']).subscribe((product: Product) => {
+        this.product = product;
+      });
     });
   }
-
 }
--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { ProductService } from '../../shared/services/product.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-edit',
@@ -12,13 +12,16 @@ export class ProductEditComponent implements OnInit {
   product: Product;
 
   constructor(
+    private route: ActivatedRoute,
     private router: Router,
     private productService: ProductService,
   ) {}
 
   ngOnInit() {
-    this.productService.get(2).subscribe((product: Product) => {
-      this.product = product;
+    this.route.params.subscribe((params: Params) => {
+      this.productService.get(params['id']).subscribe((product: Product) => {
+        this.product = product;
+      });
     });
   }
 
