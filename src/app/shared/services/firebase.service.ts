2105507a5c50613b7c56d221016fe6aaf2350d42
Zenn <app.zenn@gmail.com>
2018-08-23 02:36:32 +0900

    トークンをコンソールに表示

--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -24,10 +24,14 @@ export class FirebaseService {
 
   signInOrCreateUser(email, password): void {
     firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential: UserCredential) => {
-      console.log(userCredential.user.uid);
+      firebase.auth().currentUser.getIdToken().then((token: string) => {
+        console.log(token);
+      });
     }).catch(() => {
-      firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential: UserCredential) => {
-        console.log(userCredential.user.uid);
+      return firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential: UserCredential) => {
+        firebase.auth().currentUser.getIdToken().then((token: string) => {
+          console.log(token);
+        });
       });
     });
   }
